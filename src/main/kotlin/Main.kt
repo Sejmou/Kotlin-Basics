0c585a302e5656f56d46af8f5ab96b0929b9829c
Sejmou <samo.kolter@gmail.com>
2022-02-06 19:05:18 +0100

    illustrating conciseness of Kotlin code lol

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,71 @@
+import java.util.*
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+/**
+ * Return food for fish for a given day of the week.
+ *
+ * @param day The day of the week
+ */
+fun fishFood (day : String) : String {
+    //Not very concise code
+//    var food = ""
+//    when (day) {
+//        "Monday" -> food = "flakes"
+//        "Tuesday" -> food = "pellets"
+//        "Wednesday" -> food = "redworms"
+//        "Thursday" -> food = "granules"
+//        "Friday" -> food = "mosquitoes"
+//        "Saturday" -> food = "lettuce"
+//        "Sunday" -> food = "plankton"
+//    }
+//    return food
+
+    // more concise - use val as value is assigned exactly once
+//    val food : String
+//    when (day) {
+//        "Monday" -> food = "flakes"
+//        "Wednesday" -> food = "redworms"
+//        "Thursday" -> food = "granules"
+//        "Friday" -> food = "mosquitoes"
+//        "Sunday" -> food = "plankton"
+//        else -> food = "nothing"
+//    }
+//    return food
+
+
+    // more concise
+//    val food : String = when (day) {
+//        "Monday" -> "flakes"
+//        "Wednesday" -> "redworms"
+//        "Thursday" -> "granules"
+//        "Friday" -> "mosquitoes"
+//        "Sunday" -> "plankton"
+//        else -> "nothing"
+//    }
+//    return food
+
+    // pro-level conciseness
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println ("Today is $day and the fish eat $food")
+}
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
