96d960e1844c80d26d035115d35a3c3967ac8e52
Sejmou <samo.kolter@gmail.com>
2022-02-07 12:09:26 +0100

    simple demo for when statement

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,44 +12,6 @@ fun randomDay() : String {
  * @param day The day of the week
  */
 fun fishFood (day : String) : String {
-    //Not very concise code
-//    var food = ""
-//    when (day) {
-//        "Monday" -> food = "flakes"
-//        "Tuesday" -> food = "pellets"
-//        "Wednesday" -> food = "redworms"
-//        "Thursday" -> food = "granules"
-//        "Friday" -> food = "mosquitoes"
-//        "Saturday" -> food = "lettuce"
-//        "Sunday" -> food = "plankton"
-//    }
-//    return food
-
-    // more concise - use val as value is assigned exactly once
-//    val food : String
-//    when (day) {
-//        "Monday" -> food = "flakes"
-//        "Wednesday" -> food = "redworms"
-//        "Thursday" -> food = "granules"
-//        "Friday" -> food = "mosquitoes"
-//        "Sunday" -> food = "plankton"
-//        else -> food = "nothing"
-//    }
-//    return food
-
-
-    // more concise
-//    val food : String = when (day) {
-//        "Monday" -> "flakes"
-//        "Wednesday" -> "redworms"
-//        "Thursday" -> "granules"
-//        "Friday" -> "mosquitoes"
-//        "Sunday" -> "plankton"
-//        else -> "nothing"
-//    }
-//    return food
-
-    // pro-level conciseness
     return when (day) {
         "Monday" -> "flakes"
         "Wednesday" -> "redworms"
@@ -62,14 +24,10 @@ fun fishFood (day : String) : String {
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println ("Today is $day and the fish eat $food")
 }
 
 fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    feedTheFish()
 }
