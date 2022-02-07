2400c7d49496796127a39fe8a42139f13d589976
Sejmou <samo.kolter@gmail.com>
2022-02-07 15:41:32 +0100

    subclass example

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,15 +1,22 @@
 package example.myapp
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import java.lang.Math.PI
+
+open class Aquarium(open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     init {
         println("aquarium initializing")
     }
 
-    var volume: Int
+    open var volume: Int
         get() = width * height * length / 1000  // 1000 cm^3 = 1 l
         set(value) {//adapt height depending on provided volume
             height = (value * 1000) / (width * length)
         }
+
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
 
     constructor(numberOfFish: Int) : this() {
         // 2,000 cm^3 per fish + extra room so water doesn't spill
@@ -20,9 +27,23 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     }
 
     fun printSize() {
+        println("Shape: $shape")
         println("Width: $width cm " +
             "Length: $length cm " +
             "Height: $height cm ")
-        println("Volume: ${volume}l")
+        println("Volume: ${volume}l, Water: ${water}l (${water/volume*100.0}% full)")
     }
+}
+
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+
+    override var water = volume * 0.8
+
+    override val shape = "cylinder"
 }
