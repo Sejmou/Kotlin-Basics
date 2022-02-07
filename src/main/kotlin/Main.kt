a24304eaf6402d2664b6a109f60f4343e4a08e9b
Sejmou <samo.kolter@gmail.com>
2022-02-07 12:22:18 +0100

    lazy filter demo (with map)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,21 @@
 fun main(args: Array<String>) {
     val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
-    // eager, creates a new list
-    val eager = decorations.filter { it [0] == 'p' }
-    println("eager filter: $eager")
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
 
-    // lazy, will wait until asked to evaluate
-    val lazy = decorations.asSequence().filter { it[0] == 'p' }
-    println("lazy filter (filtering Sequence): $lazy")
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
 
-    // force evaluation of the lazy list
-    val newList = lazy.toList()
-    println("evaluated lazy-filtered list: $newList")
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
 }
