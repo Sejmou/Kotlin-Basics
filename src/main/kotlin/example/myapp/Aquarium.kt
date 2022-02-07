a6938b14c0fb3722ea0ff2d84789106501018238
Sejmou <samo.kolter@gmail.com>
2022-02-07 14:00:12 +0100

    getters and setters

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -4,26 +4,25 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
     init {
         println("aquarium initializing")
     }
-    init {
-        // 1 liter = 1000 cm^3
-        println("Volume: ${width * length * height / 1000}l")
-    }
+
+    var volume: Int
+        get() = width * height * length / 1000  // 1000 cm^3 = 1 l
+        set(value) {//adapt height depending on provided volume
+            height = (value * 1000) / (width * length)
+        }
 
     constructor(numberOfFish: Int) : this() {
-        println("original size:")
-        printSize()
-
         // 2,000 cm^3 per fish + extra room so water doesn't spill
         val tank = numberOfFish * 2000 * 1.1
 
         // calculate the height needed
         height = (tank / (length * width)).toInt()
-        println("new height (to accommodate $numberOfFish fish): $height cm")
     }
 
     fun printSize() {
         println("Width: $width cm " +
             "Length: $length cm " +
             "Height: $height cm ")
+        println("Volume: ${volume}l")
     }
 }
--- src/main/kotlin/example/myapp/main.kt
+++ src/main/kotlin/example/myapp/main.kt
@@ -3,7 +3,8 @@ package example.myapp
 fun buildAquarium() {
     val aquarium6 = Aquarium(numberOfFish = 29)
     aquarium6.printSize()
-    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000}l")
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
 
 fun main() {
