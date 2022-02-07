69e1b340ae478e05a52ae82e5fd6523701626c03
Sejmou <samo.kolter@gmail.com>
2022-02-07 13:52:34 +0100

    secondary constructor demo

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -9,6 +9,18 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
         println("Volume: ${width * length * height / 1000}l")
     }
 
+    constructor(numberOfFish: Int) : this() {
+        println("original size:")
+        printSize()
+
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+        println("new height (to accommodate $numberOfFish fish): $height cm")
+    }
+
     fun printSize() {
         println("Width: $width cm " +
             "Length: $length cm " +
