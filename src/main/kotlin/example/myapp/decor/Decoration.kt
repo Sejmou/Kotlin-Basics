c8cd857f132e12404ffcb1850de0d2ecb51f668d
Sejmou <samo.kolter@gmail.com>
2022-02-07 17:34:53 +0100

    enums

--- src/main/kotlin/example/myapp/decor/Decoration.kt
+++ src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -11,6 +11,17 @@ fun makeDecorations() {
 
 data class Decoration(val rocks: String, val diver: String)
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
+}
+
 fun main() {
-    makeDecorations()
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+    println("${Color.RED.name}: ${Color.RED.rgb}")
 }
