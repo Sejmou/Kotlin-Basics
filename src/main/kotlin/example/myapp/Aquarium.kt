f90b99d79d40e26e7bf8d30174420b1f96a7c80d
Sejmou <samo.kolter@gmail.com>
2022-02-07 13:44:47 +0100

    init blocks

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,6 +1,14 @@
 package example.myapp
 
 class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000}l")
+    }
+
     fun printSize() {
         println("Width: $width cm " +
             "Length: $length cm " +
